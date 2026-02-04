89e02eef37e98798aa90af186c83510cea1d513f
Andrew Vreeland <andrewvreeland@gmail.com>
2026-02-03 20:25:22 -0500

    finished lab assignment, need to add comments

--- java/assignment1/assignment1/src/Vreeland_Andrew.java
+++ java/assignment1/assignment1/src/Vreeland_Andrew.java
@@ -1,27 +1,86 @@
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Andrew Vreeland TODO: must update PID and NID *PID:j1234567
  *
- *  *NID:jo123456
+ * *NID:jo123456
  *
- *  *This program is done for COP3330 course at year 2026. This program created
+ * *This program is done for COP3330 course at year 2026. This program creates
  * a formatted receipt.
  */
 public class Vreeland_Andrew {
 
+    static final double TAX_RATE = 0.30;
+
     public static void main(String[] args) {
-        StringBuilder sb;
 
         Random random = new Random();
-        int upperBound = 2000; // exclusive
-        int lowerBound = 1000; // inclusive
-        int intRandom = random.nextInt(lowerBound, upperBound);
+        int reciptUpperBound = 2000; // exclusive
+        int reciptLowerBound = 1000; // inclusive
+        int randomReciptNum = random.nextInt(reciptLowerBound, reciptUpperBound);
 
+        printRecipt(randomReciptNum);
+    }
+
+    private static void userInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        double totalCost = 0;
+        int itemCounter = 0;
+
+        String itemString = "Write item name ";
+        String costString = "Write price ";
+        String itemNumString = "item number ";
+
+        String userInput = scanner.nextLine();
+        while (!userInput.contains("DONE")) {
+
+            System.out.println(itemString + userInput);
+            itemCounter++;
+
+            if (userInput.contentEquals("Food") || userInput.contentEquals("food")) {
+                Double userPriceInput = scanner.nextDouble();
+                scanner.nextLine();
+                System.out.println(costString + userPriceInput);
+                totalCost += userPriceInput;
+                System.out.println(
+                    itemNumString 
+                    + itemCounter 
+                    + " "
+                    + String.format("%,.2f", userPriceInput));
+            } else {
+                Double userPriceInput = scanner.nextDouble();
+                scanner.nextLine();
+                System.out.println(costString + userPriceInput);
+                userPriceInput = userPriceInput +  (userPriceInput * TAX_RATE);
+                totalCost += userPriceInput;
+                System.out.println(
+                    itemNumString 
+                    + itemCounter 
+                    + " "
+                    + String.format("%,.2f", userPriceInput));
+            }
+            userInput = scanner.nextLine();
+        }
+        System.out.println(itemString + userInput);
+        System.out.println(itemCounter + " items      total " + String.format("%,.2f", totalCost));
+        scanner.close();
+    }
+
+    private static void printRecipt(int reciptNum) {
+        StringBuilder sb;
+        System.out.println("******************************");
+        System.out.println("****** S store ***************");
+        System.out.println("******************************");
+        System.out.println("receipt number      " + reciptNum);
         sb = generateDate();
-
         System.out.println(sb.toString());
+        userInput();
+        for (int i = 0; i < 3; i++) {
+            System.out.println("******************************");
+        }
     }
 
     public static StringBuilder generateDate() {
@@ -36,21 +95,20 @@ public class Vreeland_Andrew {
         int dayUpperBound = 27;
         int day = random.nextInt(dayLowerBound, dayUpperBound);
 
-        String monthArr[]
-                = {
-                    "January",
-                    "Febuary",
-                    "March",
-                    "April",
-                    "May",
-                    "June",
-                    "July",
-                    "August",
-                    "September",
-                    "October",
-                    "November",
-                    "December"
-                };
+        String monthArr[] = {
+                "January",
+                "Febuary",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+        };
 
         int monthLowerBound = 0;
         int monthUpperBound = 12;
@@ -58,8 +116,7 @@ public class Vreeland_Andrew {
 
         sb.append(month);
         sb.append(" ");
-        if (day < 10) 
-        {
+        if (day < 10) {
             sb.append("0");
         }
         sb.append(day);
